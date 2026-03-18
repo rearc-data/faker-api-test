7f1698f8783908dd5f8106a4e8ff20c7b57aacb2
ashwin975 <ashwinsingaram579@gmail.com>
2026-03-18 21:00:26 +0000

    record count

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import { faker } from '@faker-js/faker';
 import { writeFileSync, mkdirSync, existsSync, readdirSync } from 'fs';
 import { gzipSync } from 'zlib';
 
+RECORD_COUNT = 100
+
 const app = express();
 
 // The volume path is available via environment variable based on resource key
